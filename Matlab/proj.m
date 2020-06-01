a7786582a74aae58b8f8cfb535e31df448cbd25c
samanyl <55926619+samanyl@users.noreply.github.com>
2020-05-31 20:05:30 -0700

    proj

--- Matlab/proj.m
+++ Matlab/proj.m
@@ -1,5 +1,6 @@
 %% Read in file
-file = readmatrix("Menlo_6_N004.csv");
+lmp = readmatrix("Menlo_6_N004.csv");
+reg = readmatrix("Regulation Services.csv");
 
 %% Data inputs
 QD = 8;
@@ -9,12 +10,33 @@ gammaS = 1;
 gammaC = 0.8;
 gammaRU = 0.25;
 gammaRD = 0.25;
-T = 24;
 t = 1; % 1 hour periods
 r = 0;
 CD = 0; % maximizing revenue so assume no costs
 CR = 0;
-P = file((1:24),4); % price vector
+P = lmp(:,4); % price vector
+PRU = reg(:,6); % reg up price vector
+PRD = reg(:,4); % reg down price vector
+T = length(P);
 
 %% Linear Optimization
-opf = proj_OPF(P,CD,CR,QD,QR,S,gammaS,gammaC,gammaRU,gammaRD,T,t,r);
+opf = proj_OPF(P,PRU,PRD,CD,CR,QD,QR,S,gammaS,gammaC,gammaRU,gammaRD,T,t,r);
+
+opf.Rev
+
+%% Plot
+interval = (1:72); %x-axis
+
+h(1)=plot(interval, P(1:72));
+hold on
+h(2)=plot(interval, opf.s(1:72));
+hold on
+h(3)=plot(interval, (opf.qR(1:72) - opf.qD(1:72)));
+
+% h(3)=plot(int, opf.qR);
+% hold on
+% h(4)=plot(int, opf.qD);
+% hold on
+
+legend(h,'P ($/MWh)','s (MWh)','q (MW)');
+xlabel('Time of the Day');
--- Matlab/proj_OPF.m
+++ Matlab/proj_OPF.m
@@ -3,7 +3,7 @@
 % Inputs:
 % QD - max quantity that can be sold/discharged in a single period
 % QR - max quantity that can be bought/recharged in a single period
-% S - max stor age cappacity
+% S - max storage cappacity
 % gammaS - storeage efficiency
 % gammaC - conversion efficiency
 % gammaRU - regulation up efficiency
@@ -14,6 +14,8 @@
 % CD = cost of discharging at time t ($/MWh)
 % CR - cost of recharging at time t ($/MWh)
 % P - price of electricity (LMP) at time t ($/MWh)
+% PRU - price of regulation up at time t ($/MWh)
+% PRD - price of regulation down at time t ($/MWh)
 
 % Outputs:
 % .qR - quantity of energy purchase (recharged) at time t (MWh)
@@ -21,40 +23,44 @@
 % .qRU - quantity of energy offered into the regulation up market at time t (MWh)
 % .qRD - quantity of energy offered into the regulation up market at time t (MWh)
 % .S - state of charge at time t
-% .Cost - minimized cost
+% .Rev - maximized revenue
 % .LMP_opt - Locational marginal price [nx1]
 
-function [opf]=proj_OPF(P,CD,CR,QD,QR,S,gammaS,gammaC,gammaRU,gammaRD,T,t,r)
+function [opf]=proj_OPF(P,PRU,PRD,CD,CR,QD,QR,S,gammaS,gammaC,gammaRU,gammaRD,T,t,r)
 
 cvx_begin
     variable qD(T); % optimization variable: qD
     variable qR(T); % optimization variable: qR
-%     variable qRU(T); % optimization variable: qRU
-%     variable qRD(T); % optimization variable: qRD
     variable s(T); % optimization variable: S
+    variable qRU(T); % optimization variable: qRU
+    variable qRD(T); % optimization variable: qRD
 
-    minimize(-(((P - CD)' * qD - (P + CR)' * qR) * exp(-r * t))) % objective function
-%     minimize(-((P-CD) * qD + (P)))
+%     minimize(-(((P - CD)' * qD - (P + CR)' * qR) * exp(-r * t))) % objective function
+    minimize(-(((P - CD)' * qD + (PRU + gammaRU * (P - CD))' * qRU + (PRD - gammaRD * (P + CR))' * qRD - (P + CR)' * qR) * exp(-r * t)))
     subject to
 
     %%%% arbitrage constraints
-    s(2:T) == gammaS * s(1:T-1) + gammaC * qR(2:T) - qD(2:T);
-    0 <= s <= S;
-    0 <= qR <= QR;
-    0 <= qD <= QD;
-
-%     %%%% arbitrage and regulation constraints
-%     s(2:T) == gammaS * s(1:T-1) + gammaC * qR(1:T-1) - qD(1:T-1) + gammaC * gammaRD * qRD - gammaRU * qRU;
+%     s(2:T) == gammaS * s(1:T-1) + gammaC * qR(2:T) - qD(2:T);
+%     s(1) == 0;
+%     qD(1) == 0; % do we need to enforce this?
 %     0 <= s <= S;
-%     0 <= (qR + qRD) <= QR;
-%     0 <= (qD + qRU) <= QD;
+%     0 <= qR <= QR;
+%     0 <= qD <= QD;
+
+    %%%% arbitrage and regulation constraints
+    s(2:T) == gammaS * s(1:T-1) + gammaC * qR(2:T) - qD(2:T) + gammaC * gammaRD * qRD(2:T) - gammaRU * qRU(2:T);
+    s(1) == 0;
+    0 <= s <= S;
+    0 <= (qR + qRD) <= QR;
+    0 <= (qD + qRU) <= QD;
 
 cvx_end
 
-opf.Cost=cvx_optval;
-opf.qR=qR;
-opf.qD = qD;
-% opf.qRU=qRU;
-% opf.qRD = qRD;
+opf.Rev=-cvx_optval;
 opf.s = s;
+opf.qR = qR;
+opf.qD = qD;
+opf.qRU = qRU;
+opf.qRD = qRD;
+
 end
